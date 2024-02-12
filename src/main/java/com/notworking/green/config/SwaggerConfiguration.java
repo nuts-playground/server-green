69ac1c6ceb915deaeb3f51aacb11d8accc1c3877
minheeya <himinhee1113@gmail.com>
2024-02-13 00:55:35 +0900

    init: Swagger Configuration 생성

---
+++ src/main/java/com/notworking/green/config/SwaggerConfiguration.java
@@ -0,0 +1,26 @@
+package com.notworking.green.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("그린 API 문서")
+                .description("https://github.com/nuts-playground/server-green")
+                .version("1.0.0");
+    }
+
+}
