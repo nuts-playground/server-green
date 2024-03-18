92a30b90d49aab3da02c3a4f6671fb170a260507
minheeya <himinhee1113@gmail.com>
2024-03-18 21:36:32 +0900

    test: Junit설정 및 테스트 코드 추가

--- src/main/java/com/notworking/green/controller/HelloController.java
+++ src/main/java/com/notworking/green/controller/HelloController.java
@@ -1,12 +1,12 @@
 package com.notworking.green.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello green world";
     }
