980f5e5669ef36fddb2b8a4b9b99e3603e4a5e4a
minheeya <himinhee1113@gmail.com>
2024-03-19 21:22:20 +0900

    test: Junit설정 및 테스트 코드 추가

--- src/main/java/com/notworking/green/controller/HelloController.java
+++ src/main/java/com/notworking/green/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.notworking.green.controller;
 
+import com.notworking.green.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello green world";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
